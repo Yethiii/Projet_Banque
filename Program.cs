46cb63d9c605e88211ab7657dbfe888add5d9884
Laetitia <laetitia.voue@gmail.com>
2024-10-12 13:00:37 +0200

    Commit_Exo3

--- Program.cs
+++ Program.cs
@@ -1,6 +1,43 @@
-﻿public class Person
+﻿var MyClient = new Person("Laetitia", "VOUE", new DateTime(1990,05,02));
+
+public class Person (string fname, string lname, DateTime date)
 {
-    public string FirstName;
-    public string LastName;
-    public DateTime BirthDate;
+    public string FirstName {get;set;}
+    public string LastName {get;set;}
+    public DateTime BirthDate {get;set;}
+}
+
+public class CurrentAccount //CompteCourant
+{
+    public string Number{get;set;}
+    public double Balance {get; private set; } 
+    public double CreditLine{get;set;} 
+    public Person Owner{get;set;}
+
+    public void Withdraw(double amount)
+    {
+        Balance -= amount
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+}
+
+public class Bank
+{
+    private readonly Dictionary<string, CurrentAccount> Account;
+    private string Name;
+
+    public void AddAccount(CurrentAccount account)
+    {
+        
+    }
+
+    public void DeleteAccount(string number)
+    {
+        
+    }
 }
